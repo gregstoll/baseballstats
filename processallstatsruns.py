935dacdfbfaa5721bac63a9d3c0d1eaf2edd2854
Greg Stoll <greg@gregstoll.com>
2020-08-24 21:20:27 -0500

    add cumulative ballsstrikes data for runs per inning

--- processallstatsruns.py
+++ processallstatsruns.py
@@ -8,16 +8,25 @@ def run(args, output_file_name):
         subprocess.run(args, check=True, universal_newlines=True, stdout=output_file)
 
 run(['./processstatsruns.py', 'runsperinningstats'], 'runsperinning.xml')
+run(['./processballsstrikesstatsruns.py', 'runsperinningballsstrikesstats'], 'runsperinningballsstrikes.xml')
 try:
     os.mkdir('statsruns')
 except FileExistsError:
     # directory already exists
     pass
 file_name_re = re.compile('runsperinningstatscumulative\.(\d+)')
+file_name_balls_strikes_re = re.compile('runsperinningballsstrikesstatscumulative\.(\d+)')
 stats_years_dir = os.path.abspath('statsyears')
 for file_name in os.listdir(stats_years_dir):
     match = file_name_re.match(file_name)
     if match:
-        output_file_name = os.path.join(os.path.abspath('statsruns'), 'runsperinningcumulative' + match.group(1) + '.xml')
+        year = match.group(1)
+        output_file_name = os.path.join(os.path.abspath('statsruns'), 'runsperinningcumulative' + year + '.xml')
         run(['./processstatsruns.py', os.path.join(os.path.abspath('statsyears'), file_name)], output_file_name)
-        print(match.group(1))
+        print(year)
+    balls_strikes_match = file_name_balls_strikes_re.match(file_name)
+    if balls_strikes_match:
+        year = balls_strikes_match.group(1)
+        output_file_name = os.path.join(os.path.abspath('statsruns'), 'runsperinningballsstrikescumulative' + year + '.xml')
+        run(['./processballsstrikesstatsruns.py', os.path.join(os.path.abspath('statsyears'), file_name)], output_file_name)
+        print(year)
---
+++ processcumulativeballsstrikesrunsperinningstats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import sys, re, os, os.path, functools
+
+lineRe = re.compile(r'^(\(\d+, \(\d+, \d+, \d+\), \(\d+, \d+\)\)): \[(.*?)\]\s*')
+fileNameRe = re.compile(r'^runsperinningballsstrikesstats\.(\d+)$')
+def main(directory):
+    fileNames = [x for x in os.listdir(directory) if fileNameRe.match(x)]
+    fileNames = sorted(fileNames)
+    print(fileNames)
+    existingLineMap = {}
+    startYear = int(fileNameRe.match(fileNames[0]).group(1))
+    endYear = int(fileNameRe.match(fileNames[-1]).group(1))
+    for year in range(startYear, endYear+1):
+        fileName = 'runsperinningballsstrikesstats.' + str(year)
+        f = None
+        try:
+            f = open(os.path.join(directory, fileName), 'r')
+        except:
+            pass
+        linesToWrite = []
+        if f != None:
+            for line in f.readlines():
+                lineMatch = lineRe.match(line)
+                if lineMatch:
+                    key = lineMatch.group(1)
+                    runDistribution = [int(x.strip()) for x in lineMatch.group(2).split(',')]
+                    if key not in existingLineMap:
+                        existingLineMap[key] = []
+                    existingRuns = existingLineMap[key]
+                    while len(existingRuns) < len(runDistribution):
+                        existingRuns.append(0)
+                    for i in range(len(runDistribution)):
+                        existingRuns[i] += runDistribution[i]
+                    existingLineMap[key] = existingRuns
+                    linesToWrite.append(key + ": [" + ', '.join([str(x) for x in existingRuns]) + "]")
+                else:
+                    print("ERROR - couldn't parse line %s" % line)
+            f.close()
+        linesToWrite.sort()
+        with open(os.path.join(directory, 'runsperinningballsstrikesstatscumulative.' + str(year)), 'w') as outFile:
+            for line in linesToWrite:
+                outFile.write(line + '\n')
+
+if (__name__ == '__main__'):
+    main(sys.argv[1])
