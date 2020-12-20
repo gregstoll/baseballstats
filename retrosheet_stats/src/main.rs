f3e3a91e78955985802a0e2e31993ccc42d31ebd
gregstoll <greg@gregstoll.com>
2020-12-19 22:00:27 -0600

    adding next_inning_if_three_outs

--- retrosheet_stats/src/main.rs
+++ retrosheet_stats/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 enum Verbosity {
     Quiet = 0,
     Normal = 1,
@@ -13,6 +15,25 @@ struct GameSituation {
     is_home: bool,
 }
 
+impl GameSituation {
+    fn next_inning_if_three_outs(self: &mut Self) {
+        if self.outs >= 3 {
+            if self.is_home {
+                self.is_home = false;
+                self.inning += 1;
+            }
+            else {
+                self.is_home = true;
+            }
+            self.outs = 0;
+            self.runners[0] = false;
+            self.runners[1] = false;
+            self.runners[2] = false;
+            self.cur_score_diff = -1 * self.cur_score_diff;
+        }
+    }
+}
+
 // TODO - parallel
 
 fn main() {
@@ -21,10 +42,86 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    #![allow(non_snake_case)]
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(0, 0);
+    fn test_next_inning_if_three_outs__zero_outs() {
+        let orig_inning = GameSituation {
+            cur_score_diff: 2,
+            inning: 1,
+            is_home: false,
+            outs: 0,
+            runners: [false, true, false]
+        };
+        let mut new_inning = orig_inning.clone();
+        new_inning.next_inning_if_three_outs();
+        assert_eq!(orig_inning, new_inning);
+    }
+
+    #[test]
+    fn test_next_inning_if_three_outs__one_out() {
+        let orig_inning = GameSituation {
+            cur_score_diff: 2,
+            inning: 1,
+            is_home: false,
+            outs: 1,
+            runners: [false, true, false]
+        };
+        let mut new_inning = orig_inning.clone();
+        new_inning.next_inning_if_three_outs();
+        assert_eq!(orig_inning, new_inning);
+    }
+
+    #[test]
+    fn test_next_inning_if_three_outs__two_outs() {
+        let orig_inning = GameSituation {
+            cur_score_diff: 2,
+            inning: 1,
+            is_home: false,
+            outs: 2,
+            runners: [false, true, false]
+        };
+        let mut new_inning = orig_inning.clone();
+        new_inning.next_inning_if_three_outs();
+        assert_eq!(orig_inning, new_inning);
+    }
+
+    #[test]
+    fn test_next_inning_if_three_outs__three_outs_home() {
+        let mut orig_inning = GameSituation {
+            cur_score_diff: 2,
+            inning: 1,
+            is_home: true,
+            outs: 3,
+            runners: [false, true, false]
+        };
+        orig_inning.next_inning_if_three_outs();
+        assert_eq!(GameSituation {
+            cur_score_diff: -2,
+            inning: 2,
+            is_home: false,
+            outs: 0,
+            runners: [false, false, false]
+        }, orig_inning);
+    }
+
+    #[test]
+    fn test_next_inning_if_three_outs__three_outs_visitor() {
+        let mut orig_inning = GameSituation {
+            cur_score_diff: 2,
+            inning: 1,
+            is_home: false,
+            outs: 3,
+            runners: [false, true, false]
+        };
+        orig_inning.next_inning_if_three_outs();
+        assert_eq!(GameSituation {
+            cur_score_diff: -2,
+            inning: 1,
+            is_home: true,
+            outs: 0,
+            runners: [false, false, false]
+        }, orig_inning);
     }
 }
