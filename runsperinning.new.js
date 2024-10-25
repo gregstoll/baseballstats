5cd741552b3f3dc31e83afd119c1dad961dc5769
Greg Stoll <greg@gregstoll.com>
2024-10-24 21:10:03 -0500

    starting runsperinning work

---
+++ runsperinning.new.js
@@ -0,0 +1,98 @@
+//<![CDATA[
+
+let runsXML = null;
+var isIE = null;
+
+function updateProbabilities() {
+    var outs = $F('outs');
+    var runners = $F('runners');
+    if (!runsXML) {
+        // wait for updateRunsXML() to finish, then this will be called again
+        return;
+    }
+    var xpathExpr = `//situation[@outs="${outs}" and @runners="${runners}"]`;
+    var result = runsXML.evaluate(xpathExpr, runsXML, null, XPathResult.ANY_TYPE, null);
+    if (result.snapshotLength > 0) {
+        clearAll($('outTbody'));
+        var headerRow = document.createElement('tr');
+        var headerTh = document.createElement('th');
+        headerRow.appendChild(headerTh);
+        headerTh = document.createElement('th');
+        headerTh.appendChild(document.createTextNode('Number'));
+        headerRow.appendChild(headerTh);
+        headerTh = document.createElement('th');
+        headerTh.appendChild(document.createTextNode('Probability'));
+        headerRow.appendChild(headerTh);
+        $('outTbody').appendChild(headerRow);
+        
+        var element = elems[0];
+        var elementXPath = getXPathFromXML(element);
+        // FFV - I can''t get these two to play together.
+        // 'total' doesn''t return anything for IE, while
+        // '/situation/total' doesn''t for mozilla, and
+        // '//total' (bizarrely) returns the first total
+        // in the xml document, which is scary.
+        xpathExpr = getNestedXPathExpression('situation', 'total');
+        var total = parseInt(getDOMText(elementXPath.selectNodes(xpathExpr)[0].childNodes));
+
+        var totalRow = document.createElement('tr');
+        var totalHeader = document.createElement('th');
+        totalHeader.appendChild(document.createTextNode('Total'));
+        totalRow.appendChild(totalHeader);
+        var totalTd = document.createElement('td');
+        totalTd.appendChild(document.createTextNode(total));
+        totalRow.appendChild(totalTd);
+        var totalTd = document.createElement('td');
+        totalTd.appendChild(document.createTextNode('100.00000%'));
+        totalRow.appendChild(totalTd);
+        $('outTbody').appendChild(totalRow);
+        var runArray = new Array();
+        var runElems = element.selectNodes('count');
+        var expectedTotal = 0.0;
+        for (var i = 0; i < runElems.length; i++) {
+            var curRunElem = runElems[i];
+            var curNum = parseInt(getDOMText(curRunElem.childNodes));
+            expectedTotal += (curNum * i);
+            var curProb = (curNum/total) * 100.0; 
+            var curRow = document.createElement('tr');
+            var curHeader = document.createElement('th');
+            curHeader.appendChild(document.createTextNode(i + ' runs'));
+            curRow.appendChild(curHeader);
+            var curTd = document.createElement('td');
+            curTd.appendChild(document.createTextNode(curNum));
+            curRow.appendChild(curTd);
+            var curTd = document.createElement('td');
+            curTd.appendChild(document.createTextNode(curProb.toFixed(5) + '%'));
+            curRow.appendChild(curTd);
+            $('outTbody').appendChild(curRow);
+        }
+        var expected = (expectedTotal/total);
+        clearAll($('outExpected'));
+        $('outExpected').appendChild(document.createTextNode(expected.toFixed(5)));
+    }
+}
+
+function doRequest(url, async, callback, method, postData) {
+    var xmlhttp = new Ajax.Request(
+        url,
+        {
+            method: method,
+            parameters: '',
+            onComplete: callback 
+        });
+}
+
+async function updateRunsXML() {
+    const response = await fetch("runsperinning.xml");
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const text = await response.text();
+    const parser = new DOMParser();
+    runsXML = parser.parseFromString(text, "application/xml");
+    updateProbabilities();
+}
+updateRunsXML();
+
+
+//]]>
