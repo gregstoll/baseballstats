b2f49831abd9a9e0d7b215423b3d992fb8dafaa8
Greg Stoll <greg@gregstoll.com>
2024-10-25 20:54:47 -0500

    update runsperinning page to use modern XPath stuff (and no prototype)

--- runsperinning.js
+++ runsperinning.js
@@ -1,22 +1,21 @@
 //<![CDATA[
 
-var runsXML = null;
-var isIE = null;
-
-function processResponse(originalRequest) {
-    var outs = $F('outs');
-    var runners = $F('runners');
-    if (runsXML == null) {
-        runsXML = originalRequest.responseXML.documentElement;
+let runsXML = null;
+function updateProbabilities() {
+    if (!runsXML) {
+        // wait for updateRunsXML() to finish, then this will be called again
+        return;
     }
-    var docElem = runsXML;
-    var xpath = getXPathFromXML(docElem);
-    var xpathExpr = '//situation[@outs="' + outs + '" and @runners="' + runners + '"]';
-    var elems = xpath.selectNodes(xpathExpr);
-    if (elems.length > 0) {
-        clearAll($('outTbody'));
-        var headerRow = document.createElement('tr');
-        var headerTh = document.createElement('th');
+    let outs = document.getElementById('outs').value;
+    let runners = document.getElementById('runners').value;
+    let xpathExpr = `//situation[@outs="${outs}" and @runners="${runners}"]`;
+    let result = runsXML.evaluate(xpathExpr, runsXML, null, XPathResult.ANY_TYPE, null);
+    let situationNode = result.iterateNext();
+    if (situationNode) {
+        let outTBody = document.getElementById('outTbody');
+        outTBody.innerHTML = '';
+        let headerRow = document.createElement('tr');
+        let headerTh = document.createElement('th');
         headerRow.appendChild(headerTh);
         headerTh = document.createElement('th');
         headerTh.appendChild(document.createTextNode('Number'));
@@ -24,73 +23,67 @@ function processResponse(originalRequest) {
         headerTh = document.createElement('th');
         headerTh.appendChild(document.createTextNode('Probability'));
         headerRow.appendChild(headerTh);
-        $('outTbody').appendChild(headerRow);
+        outTbody.appendChild(headerRow);
         
-        var element = elems[0];
-        var elementXPath = getXPathFromXML(element);
-        // FFV - I can''t get these two to play together.
-        // 'total' doesn''t return anything for IE, while
-        // '/situation/total' doesn''t for mozilla, and
+        // FFV - I can't get these two to play together.
+        // 'total' doesn't return anything for IE, while
+        // '/situation/total' doesn't for mozilla, and
         // '//total' (bizarrely) returns the first total
         // in the xml document, which is scary.
-        xpathExpr = getNestedXPathExpression('situation', 'total');
-        var total = parseInt(getDOMText(elementXPath.selectNodes(xpathExpr)[0].childNodes));
+        let totalNode = runsXML.evaluate('total', situationNode, null, XPathResult.ANY_TYPE, null).iterateNext();
+        let total = parseInt(totalNode.innerHTML, 10);
 
-        var totalRow = document.createElement('tr');
-        var totalHeader = document.createElement('th');
+        let totalRow = document.createElement('tr');
+        let totalHeader = document.createElement('th');
         totalHeader.appendChild(document.createTextNode('Total'));
         totalRow.appendChild(totalHeader);
-        var totalTd = document.createElement('td');
+        let totalTd = document.createElement('td');
         totalTd.appendChild(document.createTextNode(total));
         totalRow.appendChild(totalTd);
-        var totalTd = document.createElement('td');
+        totalTd = document.createElement('td');
         totalTd.appendChild(document.createTextNode('100.00000%'));
         totalRow.appendChild(totalTd);
-        $('outTbody').appendChild(totalRow);
-        var runArray = new Array();
-        var runElems = element.selectNodes('count');
-        var expectedTotal = 0.0;
-        for (var i = 0; i < runElems.length; i++) {
-            var curRunElem = runElems[i];
-            var curNum = parseInt(getDOMText(curRunElem.childNodes));
+        outTbody.appendChild(totalRow);
+        let runArray = [];
+        let runElems = runsXML.evaluate('count', situationNode, null, XPathResult.ANY_TYPE, null);
+        let expectedTotal = 0.0;
+        let i = 0;
+        let curRunElem = null;
+        while (curRunElem = runElems.iterateNext()) {
+            let curNum = parseInt(curRunElem.innerHTML, 10);
             expectedTotal += (curNum * i);
-            var curProb = (curNum/total) * 100.0; 
-            var curRow = document.createElement('tr');
-            var curHeader = document.createElement('th');
+            let curProb = (curNum/total) * 100.0; 
+            let curRow = document.createElement('tr');
+            let curHeader = document.createElement('th');
             curHeader.appendChild(document.createTextNode(i + ' runs'));
             curRow.appendChild(curHeader);
-            var curTd = document.createElement('td');
+            let curTd = document.createElement('td');
             curTd.appendChild(document.createTextNode(curNum));
             curRow.appendChild(curTd);
-            var curTd = document.createElement('td');
+            curTd = document.createElement('td');
             curTd.appendChild(document.createTextNode(curProb.toFixed(5) + '%'));
             curRow.appendChild(curTd);
-            $('outTbody').appendChild(curRow);
+            outTbody.appendChild(curRow);
+            i++;
         }
-        var expected = (expectedTotal/total);
-        clearAll($('outExpected'));
-        $('outExpected').appendChild(document.createTextNode(expected.toFixed(5)));
+        let expected = (expectedTotal/total);
+        let outExpected = document.getElementById('outExpected');
+        outExpected.innerHTML = '';
+        outExpected.appendChild(document.createTextNode(expected.toFixed(5)));
     }
 }
 
-function updateProbabilities() {
-    if (runsXML == null) {
-        doRequest('runsperinning.xml', true, processResponse, 'GET', null);
-    } else {
-        processResponse(null);
+async function updateRunsXML() {
+    const response = await fetch("runsperinning.xml");
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+    const text = await response.text();
+    const parser = new DOMParser();
+    runsXML = parser.parseFromString(text, "application/xml");
+    updateProbabilities();
 }
+updateRunsXML();
 
-function doRequest(url, async, callback, method, postData) {
-    var xmlhttp = new Ajax.Request(
-        url,
-        {
-            method: method,
-            parameters: '',
-            onComplete: callback 
-        });
-}
-
-updateProbabilities();
 
 //]]>
